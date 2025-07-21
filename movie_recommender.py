d3d2e65759131493174dcd6b862ef88411e39167
Tathastu Mishra <tathastumishra2020@gmail.com>
2025-07-21 18:49:58 +0530

    movie_recommender.py

--- movie_recommender.py
+++ movie_recommender.py
@@ -1,6 +1,7 @@
 import ast
 import pandas as pd
 import numpy as np
+import nltk
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
